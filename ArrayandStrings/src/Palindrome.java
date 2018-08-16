777cbbc33d2c87bb5c76eacfd685f89e7da07fb8
sara bahrami <sarabahrami83@gmail.com>
2018-08-15 23:41:51 -0700

    Given a string, write a function to check if it is a permutation of a palindrome

--- ArrayandStrings/src/Palindrome.java
+++ ArrayandStrings/src/Palindrome.java
@@ -4,6 +4,44 @@
 // to be limited to just dictionary words. EXAMPLE
         //Input: Tact Coa
        // Output: True (permutations: "taco cat". "atco cta". etc.) Hints: #106, #121, #134, #136
+//solution: To be more precise, strings with even length (after removing all non-letter
+// characters) must have all even counts of characters. Strings of an odd length must
+// have exactly one character with an odd count.
+
+import java.util.Arrays;
 
 public class Palindrome {
+    public static void main (String args[]) {
+        String str="tactcoapapa";
+        if (PalindromPermutation(str))
+            System.out.println("string is a PalindromPermutation");
+            else
+            System.out.println("string is not a PalindromPermutation");
+
+    }
+    public static boolean PalindromPermutation(String str) {
+        int [] frequency=new int [256];
+        Arrays.fill(frequency,0);
+        for (int i=0; i< str.length();i++){
+            frequency[(int)str.charAt(i)]++;
+        }
+
+        return checkIsEvenOrOdd(frequency);
+
+
+    }
+
+    public static boolean checkIsEvenOrOdd(int []frequency) {
+        boolean foundodd=false;
+        for (int i=0; i< frequency.length;i++){
+            if (frequency[i]%2!=0){
+                if(foundodd){
+                    return false;
+                }
+               foundodd=true;
+            }
+
+        }
+        return true;
+    }
 }
